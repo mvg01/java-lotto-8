d2f29e1f0fcf94954a587badc180423a97701979
donggeon <dkms625@naver.com>
2025-10-28 23:29:38 +0900

    feat(application): 프로그램 시작 진입점

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.controller.LottoController;
+
+/**
+ * 프로그램 실행 진입점 클래스입니다.
+ */
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoController controller = new LottoController();
+        controller.run();
     }
 }
