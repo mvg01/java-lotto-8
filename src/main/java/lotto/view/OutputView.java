ca4f12d829c3276f9f27c2d8dd1a249075592ee7
donggeon <dkms625@naver.com>
2025-10-28 23:28:26 +0900

    feat(view): 화면 출력 관련 기능 구현 및 메시지 출력 메서드 추가

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,86 @@
+package lotto.view;
+
+import java.util.List;
+
+/**
+ * 화면 출력 관련 기능을 담당하는 클래스입니다.
+ */
+public class OutputView {
+    public OutputView() {
+    }
+
+    /**
+     * 구매 금액 입력 안내 메시지를 출력합니다.
+     */
+    public void inputPurchasePrice() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    /**
+     * 구매한 로또 수량을 출력합니다.
+     *
+     * @param count 구매한 로또 갯수
+     */
+    public void outputPurchaseCount(int count) {
+        System.out.println();
+        System.out.println(count +"개를 구매했습니다.");
+    }
+
+    /**
+     * 발행된 로또 번호를 출력합니다.
+     *
+     * @param numbers 로또 번호 리스트
+     */
+    public void outputPurchasedLotto(List<Integer> numbers) {
+        System.out.print("[");
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.print(numbers.get(i));
+            if (i != numbers.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+
+    /**
+     * 당첨 번호 입력 메시지를 출력합니다.
+     */
+    public void inputWinningLotto() {
+        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    /**
+     * 보너스 번호 입력 메시지를 출력합니다.
+     */
+    public void inputBonusLotto() {
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
+    /**
+     * 결과를 출력합니다.
+     * @param resultList 일치 개수 배열
+     * @param rateOfReturn 수익률
+     */
+    public void outputResult(int[] resultList, double rateOfReturn) {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - "+resultList[0]+"개");
+        System.out.println("4개 일치 (50,000원) - "+resultList[1]+"개");
+        System.out.println("5개 일치 (1,500,000원) - "+resultList[2]+"개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+resultList[3]+"개");
+        System.out.println("6개 일치 (2,000,000,000원) - "+resultList[4]+"개");
+        System.out.println("총 수익률은 "+rateOfReturn+"%입니다.");
+    }
+
+    /**
+     * 에러 메시지 출력
+     *
+     * @param error 에러 메시지
+     */
+    public void printError(String error) {
+        System.out.println("[ERROR] : "+ error);
+    }
+}
